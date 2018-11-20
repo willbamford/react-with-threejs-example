c8c0d19142b991f92220b1502891e8b3ff5df942
Will Bamford <w.bamford@gmail.com>
2018-11-20 08:48:19 +0000

    Handle start/stop with useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
-import Vis from './VisWithClass'
-// import Vis from './VisWithHooks'
+// import Vis from './VisWithClass'
+import Vis from './VisWithHooks'
 
 const App = () => (
   <div className="app">
--- src/VisWithHooks.jsx
+++ src/VisWithHooks.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import * as THREE from 'three'
 
 const VisWithHooks = () => {
-  const mountRef = useRef(null)
+  const mount = useRef(null)
+  const [isAnimating, setAnimating] = useState(true)
+  const controls = useRef(null)
 
   useEffect(() => {
-    let width = mountRef.current.clientWidth
-    let height = mountRef.current.clientHeight
+    let width = mount.current.clientWidth
+    let height = mount.current.clientHeight
     let frameId
 
     const scene = new THREE.Scene()
@@ -21,16 +23,17 @@ const VisWithHooks = () => {
     renderer.setClearColor('#000000')
     renderer.setSize(width, height)
 
+    const renderScene = () => {
+      renderer.render(scene, camera)
+    }
+
     const handleResize = () => {
-      width = mountRef.current.clientWidth
-      height = mountRef.current.clientHeight
+      width = mount.current.clientWidth
+      height = mount.current.clientHeight
       renderer.setSize(width, height)
       camera.aspect = width / height
       camera.updateProjectionMatrix()
-    }
-
-    const renderScene = () => {
-      renderer.render(scene, camera)
+      renderScene()
     }
 
     const animate = () => {
@@ -49,16 +52,19 @@ const VisWithHooks = () => {
 
     const stop = () => {
       cancelAnimationFrame(frameId)
+      frameId = null
     }
 
-    mountRef.current.appendChild(renderer.domElement)
+    mount.current.appendChild(renderer.domElement)
     window.addEventListener('resize', handleResize)
     start()
+
+    controls.current = { start, stop }
 
     return () => {
       stop()
       window.removeEventListener('resize', handleResize)
-      mountRef.current.removeChild(renderer.domElement)
+      mount.current.removeChild(renderer.domElement)
 
       scene.remove(cube)
       geometry.dispose()
@@ -66,7 +72,28 @@ const VisWithHooks = () => {
     }
   }, [])
 
-  return <div className="vis" ref={mountRef} />
+  useEffect(
+    () => {
+      if (isAnimating) {
+        controls.current.start()
+      } else {
+        controls.current.stop()
+      }
+    },
+    [isAnimating],
+  )
+
+  /* eslint-disable
+    jsx-a11y/click-events-have-key-events,
+    jsx-a11y/no-static-element-interactions
+  */
+  return (
+    <div
+      className="vis"
+      ref={mount}
+      onClick={() => setAnimating(!isAnimating)}
+    />
+  )
 }
 
 export default VisWithHooks
